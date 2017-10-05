59188488518958ab34522e0882f24ed63d7359d4
rahul4times <ams@Rahuls-MacBook-Air.local>
2017-10-05 10:10:25 -0700

    Solved

--- part1/server.js
+++ part1/server.js
@@ -2,9 +2,41 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 8000;
 
-app.get('/yourroute', function(req, res) {
-  res.send("stuff");
+// Challenge 1 starts here
+app.get('/hello', function(req, res) {
+  res.send("Howdy Galvanize!!!");
 });
+// Challenge 1 ends here
+
+// Challenge 2 starts here
+app.post('/create/:name', function(req, res) {
+  let input = req.params.name;
+  let name = {
+    "id":"12",
+    "name":input
+  };
+  res.json(name);
+});
+// Challenge 2 ends here
+
+// Challenge 3 starts here
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+// Challenge 3 ends here
+
+// Challenge 4 starts here
+app.get('/verify/:age', function(req, res) {
+  let inputAge = parseInt(req.params.age);
+    if(inputAge > 13){
+      res.sendStatus(200);
+    } else{
+      res.sendStatus(403);
+    }
+
+});
+// Challenge 4 ends here
+
 
 app.use(function(req, res) {
   res.sendStatus(404);
--- part2/server.js
+++ part2/server.js
@@ -1,10 +1,70 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
 var port = process.env.PORT || 8000;
 
 app.get('/yourroute', function(req, res) {
-  res.send("stuff");
+  res.send("Howdy Galvanize!!!");
 });
+
+// Challenge 1 starts here
+app.get('/create/:name/:age', function(req, res){
+  fs.readFile('./storage.json', function(err, data){
+    let temp = JSON.parse(data);
+
+    let userName = req.params.name;
+    for(let i=0; i<temp.length; i++){
+      if(userName === temp[i].name){
+        res.send('Error! User already exist. Try different name.');
+        return;
+      }
+    }
+
+    let id = temp.length + 1;
+    let myObject = {
+          "name": req.params.name,
+          "age": req.params.age,
+          "id": id
+        };
+    temp.push(myObject);
+    fs.writeFile('./storage.json', JSON.stringify(temp), function(err){
+        if(err) throw err
+        res.send('Success!');
+    });
+
+  });
+
+}); // Challenge 1 ends here
+
+
+
+// Challenge 2 starts here
+app.get('/', function(req, res){
+  fs.readFile('./storage.json', 'utf8', function(err, fileData){
+    if(err) throw err;
+    let currentData = JSON.parse(fileData);
+    res.json(currentData);
+
+  });
+}); // Challenge 2 ends here
+
+
+// Challenge 3 starts here
+app.get('/:name', function(req, res){
+  let name = req.params.name;
+  fs.readFile('./storage.json', function(err, myData){
+    let temp = JSON.parse(myData);
+    for(let i=0; i<temp.length; i++){
+      console.log(i);
+      if(name === temp[i].name){
+        res.json(temp[i]);
+        return;
+      }
+    }
+    res.sendStatus(400);
+  });
+
+}); // Challenge 3 ends here
 
 app.use(function(req, res) {
   res.sendStatus(404);
